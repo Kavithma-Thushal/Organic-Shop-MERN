af1d887827a249ef60624f20f9b2756a953abff3
Kavithma-Thushal <kavithmathushal9007@gmail.com>
2024-06-07 16:58:18 +0530

    BrowserRouter Added

--- src/views/components/Navbar/Navbar.tsx
+++ src/views/components/Navbar/Navbar.tsx
@@ -7,15 +7,16 @@ export function Navbar() {
                 <a href="https://github.com/Kavithma-Thushal" target="_blank" rel="noopener noreferrer">
                     <div className="flex items-center">
                         <img src={logo} className="mr-2" style={{height: '8vmin'}} alt="Nav-logo"/>
-                        <div className="text-lg font-bold">ORGANIC <span className="text-neonGreen">SHOP</span>
+                        <div className="text-lg font-bold">MOBILE <span className="text-neonGreen">HUB</span>
                         </div>
                     </div>
                 </a>
                 <div className="space-x-8 text-sm font-semibold">
-                    <a href="#customers" className="hover:text-neonGreen transition-colors duration-300">CUSTOMERS</a>
-                    <a href="#items" className="hover:text-neonGreen transition-colors duration-300">ITEMS</a>
-                    <a href="#orders" className="hover:text-neonGreen transition-colors duration-300">ORDERS</a>
-                    <a href="#orderDetails" className="hover:text-neonGreen transition-colors duration-300">ORDER
+                    <a href="dashboard" className="hover:text-neonGreen transition-colors duration-300">DASHBOARD</a>
+                    <a href="customer" className="hover:text-neonGreen transition-colors duration-300">CUSTOMER</a>
+                    <a href="item" className="hover:text-neonGreen transition-colors duration-300">INVENTORY</a>
+                    <a href="orders" className="hover:text-neonGreen transition-colors duration-300">ORDERS</a>
+                    <a href="orderDetails" className="hover:text-neonGreen transition-colors duration-300">ORDER
                         DETAILS</a>
                 </div>
             </div>
