37c56a9e5b7811f0f1e6cbc07acb4e6d744f3a30
Kavithma-Thushal <kavithmathushal9007@gmail.com>
2024-06-12 19:13:08 +0530

    App.tsx Refactored

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 import {DefaultLayout} from "./views/components/DefaultLayout/DefaultLayout";
 import {Login} from "./views/pages/Login/Login";
