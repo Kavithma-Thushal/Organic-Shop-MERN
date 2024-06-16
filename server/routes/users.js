2afc2585454a7b45f0d98ff7c509f68dcb2e23ea
Kavithma-Thushal <kavithmathushal9007@gmail.com>
2024-06-16 15:27:23 +0530

    cors, axios, nodemon dependencies added

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,9 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', function (req, res) {
+    res.send("This is Users");
 });
 
 module.exports = router;
