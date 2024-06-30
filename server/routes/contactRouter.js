2bb85f1e8a689daecbef537769407b4f3e69b151
Kavithma-Thushal <kavithmathushal9007@gmail.com>
2024-06-30 07:53:14 +0530

    Contacts Alert Refactored

--- server/routes/contactRouter.js
+++ server/routes/contactRouter.js
@@ -4,7 +4,7 @@ var router = express.Router();
 router.post('/submit', function (req, res) {
     const requestBody = req.body;
     console.log(requestBody);
-    res.send("Contact Details Received...!");
+    res.send("Contact Details Send Successfully...!");
 });
 
 module.exports = router;
